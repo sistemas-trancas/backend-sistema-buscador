f6a72d847867c588c83a90e64a2e12ce020c17f5
zeta-dev <nvreparaciones@gmail.com>
2025-01-29 22:20:17 -0300

    Configure Mailgun SMTP settings; move credentials to environment variables for improved security

--- config/email.js
+++ config/email.js
@@ -1,12 +1,12 @@
 const nodemailer = require("nodemailer");
+require('dotenv').config();
 
-// Configuración del transporte SMTP para Mailgun
 const transporter = nodemailer.createTransport({
-  host: "smtp.mailgun.org", // Host SMTP de Mailgun
-  port: 587, // Puerto recomendado para STARTTLS
+  host: process.env.MAILGUN_HOST,
+  port: process.env.MAILGUN_PORT,
   auth: {
-    user: "postmaster@sandboxb9039b27e8ce445e8180c022ddda9502.mailgun.org", // Usuario SMTP
-    pass: "883e8010a93d71ba47691ed8613aa270-9c3f0c68-bce1a321", // Contraseña SMTP
+    user: process.env.MAILGUN_USER,
+    pass: process.env.MAILGUN_PASS,
   },
 });
 
